cd67556e004cccc3a8d6a71467c0d6d46fdf79cb
Miles Johnson <mileswjohnson@gmail.com>
2022-10-26 12:07:08 -0700

    Add function.

--- node.ts
+++ node.ts
@@ -2,19 +2,14 @@
 // @see https://github.com/actions/setup-node/blob/main/src/installer.ts
 
 import * as core from '@actions/core';
+import * as hc from '@actions/http-client';
 import * as tc from '@actions/tool-cache';
 
 function isFullyQualifiedVersion(version: string) {
 	return version.split('.').length >= 3;
 }
 
-// https://nodejs.org/dist/index.json
-// https://github.com/actions/node-versions/blob/main/versions-manifest.json
-export async function resolveVersionFromManifest(version: string) {
-	core.info('Getting manifest from actions/node-versions@main');
-
-	const manifest = await tc.getManifestFromRepo('actions', 'node-versions', undefined, 'main');
-
+async function findVersionInManifest(version: string, manifest: tc.IToolRelease[]) {
 	core.info(`Resolving version "${version}" from manifest`);
 
 	const result = await tc.findFromManifest(
@@ -30,4 +25,85 @@ export async function resolveVersionFromManifest(version: string) {
 	}
 
 	throw new Error(`Unable to find a version for value "${version}"!`);
+}
+
+// https://nodejs.org/dist/index.json
+export async function resolveVersionFromDist(version: string) {
+	core.info('Getting manifest from nodejs.org');
+
+	const httpClient = new hc.HttpClient('moonrepo/tool-version-action', [], {
+		allowRetries: true,
+		maxRetries: 3,
+	});
+	const response = await httpClient.getJson<
+		{ version: string; files: []; lts: boolean | string }[]
+	>('https://nodejs.org/dist/index.json');
+
+	// Convert dist response into a tool manifest
+	const manifest: tc.IToolRelease[] = [];
+
+	response.result?.forEach((result) => {
+		const files: tc.IToolReleaseFile[] = [];
+
+		result.files.forEach((file) => {
+			switch (file) {
+				case 'linux-x64': {
+					const filename = `node-${result.version}-linux-x64.tar.gz`;
+
+					files.push({
+						arch: 'x64',
+						download_url: `https://nodejs.org/dist/${result.version}/${filename}`,
+						filename,
+						platform: 'linux',
+					});
+					break;
+				}
+
+				case 'osx-x64-tar': {
+					const filename = `node-${result.version}-darwin-x64.tar.gz`;
+
+					files.push({
+						arch: 'x64',
+						download_url: `https://nodejs.org/dist/${result.version}/${filename}`,
+						filename,
+						platform: 'darwin',
+					});
+					break;
+				}
+
+				case 'win-x64-7z': {
+					const filename = `node-${result.version}-win-x64.zip`;
+
+					files.push({
+						arch: 'x64',
+						download_url: `https://nodejs.org/dist/${result.version}/${filename}`,
+						filename,
+						platform: 'win32',
+					});
+					break;
+				}
+
+				default:
+					break;
+			}
+		});
+
+		manifest.push({
+			files,
+			release_url: '', // not needed
+			stable: result.lts !== false,
+			version: result.version.slice(1), // remove v prefix
+		});
+	});
+
+	await findVersionInManifest(version, manifest);
+}
+
+// https://github.com/actions/node-versions/blob/main/versions-manifest.json
+export async function resolveVersionFromManifest(version: string) {
+	core.info('Getting manifest from actions/node-versions@main');
+
+	const manifest = await tc.getManifestFromRepo('actions', 'node-versions', undefined, 'main');
+
+	await findVersionInManifest(version, manifest);
 }
